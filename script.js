d6c238d3570f36b4aecd6bdca0bdb2c32f658eb0
Mark Reed <mreed@admins-mbp.mynetworksettings.com>
2023-04-18 16:06:27 -0400

    nav animation

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 let menuIsOpen = false
+function fadeInBars(){
+    document.querySelector('.bars').classList.add('fade-in')
+    document.querySelector('.bars').classList.remove('fade-out')
+}
+function fadeOutBars(){
+    document.querySelector('.bars').classList.remove('fade-in')
+    document.querySelector('.bars').classList.add('fade-out')
+}
+function slideOutNavLinks(){
+    document.querySelector('.links').classList.remove('slide-in')
+    document.querySelector('.links').classList.add('slide-out')
+}
+function slideInNavLinks(){
+    document.querySelector('.links').classList.remove('slide-out')
+    document.querySelector('.links').classList.add('slide-in')
+}
 function toggleMenu(){
     if(menuIsOpen){
-        document.querySelector('.bars').classList.add('fade-in')
-        document.querySelector('.bars').classList.remove('fade-out')
-        document.querySelector('.links').classList.remove('slide-in')
-        document.querySelector('.links').classList.add('slide-out')
+        fadeInBars()
+        slideOutNavLinks()
     }
     else{
-        document.querySelector('.bars').classList.remove('fade-in')
-        document.querySelector('.bars').classList.add('fade-out')
-        document.querySelector('.links').classList.remove('slide-out')
-        document.querySelector('.links').classList.add('slide-in')
+        fadeOutBars()
+        slideInNavLinks()
     }
     menuIsOpen = !menuIsOpen
 }
 
-document.querySelector('header nav').addEventListener('click', toggleMenu)
+document.querySelector('button.close').addEventListener('click', toggleMenu)
+document.querySelector('header .bars').addEventListener('click', toggleMenu)
